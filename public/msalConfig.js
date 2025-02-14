c82ac6827bc476c85ab2a694edb9d0dc151860df
BoyuanChen-1113 <cby2022.1113@gmail.com>
2025-02-14 21:48:02 +0800

    /

--- public/msalConfig.js
+++ public/msalConfig.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
       clientId: "367ae86e-7485-4e72-b0b3-d43392a4a16d", // e.g., "12345678-abcd-efgh-ijkl-1234567890ab"
       authority: "https://login.microsoftonline.com/cfaf56ef-ac06-42ea-9eb1-7b91c55d65af", // Change if using a tenant-specific authority
-      redirectUri: "http://localhost:3000" // e.g., "http://localhost:3000"
+      redirectUri: "https://bycms.github.io/M365Chat/public" // e.g., "http://localhost:3000"
     },
     cache: {
       cacheLocation: "localStorage", // "sessionStorage" is also available
